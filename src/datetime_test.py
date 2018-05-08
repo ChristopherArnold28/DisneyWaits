44f49e57c00a97b981b72b13e4aeacf11ef29583
Christopher.Arnold <christopher.arnold@utexas.edu>
2018-05-08 10:48:24 -0500

    Romve unnecessary Files
    Removed three files that were originally used for testing and are no longer being used.

--- src/datetime_test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from pytz import timezone
-tz = timezone('US/Eastern')
-time = datetime.now(tz)
-
-time.time
-print(time.date())
-print(time.time().isoformat(timespec = 'minutes'))
